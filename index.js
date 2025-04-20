4bfb44ca8ff1c926ae4e55f14a44aa4be244761e
Arnold <arnold.mutama@gmail.com>
2025-04-20 16:55:54 +0300

    Implements a for loop and a while loop

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+
+    let thankYouMessages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+    }
+
+    return thankYouMessages;
+  }
+
+  function countDown(myNumber) {
+
+
+    if (myNumber <= 0) {
+      console.log("not a positive integer");
+    }
+    
+    while (myNumber >= 0) {
+      console.log(myNumber);
+      myNumber--;
+    }
+  }
+
+console.log(countDown(10));
